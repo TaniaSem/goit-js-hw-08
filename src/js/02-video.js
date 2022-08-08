1881c28e7c49c5307585a8a3b2f02bf47230ad5f
TaniaSem <semenchuktetiana@gmail.com>
2022-08-08 23:57:41 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -17,19 +17,3 @@ function onPlayerTimeupdate(time) {
   console.log(currentPlayerTime);
   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, currentPlayerTime);
 }
-
-// const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-// const getSavedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
-
-// if (getSavedTime) {
-//   player.setCurrentTime(getSavedTime);
-// }
-
-// player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
-// function onTimeUpdate(time) {
-//   let savedPausedPlayerTime = time.seconds;
-//   localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, savedPausedPlayerTime);
-// }
